3a42b65c7c37723333fcc3f8f0c6aeb360f1a4cc
firstplayer34 <pas.vorobiov@gmail.com>
2020-02-08 13:03:36 +0300

    Пример с использованием структур

--- Squares/Source.cpp
+++ Squares/Source.cpp
@@ -3,39 +3,33 @@
 #include <cmath>
 using namespace std;
 
-double distance(int x1, int y1, int x2, int y2)
+struct dot
 {
-	return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-}
+	int x;
+	int y;
+};
 
-double square_of_triagle(int x1, int y1, int x2, int y2, int x3, int y3)
-{
-	double a = distance(x1, y1, x2, y2);
-	double b = distance(x2, y2, x3, y3);
-	double c = distance(x1, y1, x3, y3);
-	double p = (a + b + c) / 2;
-	return sqrt(p * (p - a) * (p - b) * (p - c));
-}
+double distance(dot A, dot B);
+double square_of_triangle(dot A, dot B, dot C);
 
 int main()
 {
-	vector<int>x;
-	vector<int>y;
-	//x[0],y[0] - координаты первой точки
+	vector<dot>input_dots;
 	int N; cin >> N;
 	for (int i = 0; i < N; i++)
 	{
 		int x1, y1;
 		cin >> x1 >> y1;
-		x.push_back(x1);
-		y.push_back(y1);
+		dot temp = {x1,y1};
+		input_dots.push_back(temp);
 	}
+	dot Zero = { 0,0 };
 	double min_square = 0;
-	for (int i = 0; i<N-2;i++)
+	for (int i = 0; i<N;i++)
 	{
 		for (int j = i+1;j<N-1;j++)
 		{
-			double current_square = square_of_triagle(x[i], y[i], x[j], y[j], 0, 0);
+			double current_square = square_of_triangle(input_dots[i], input_dots[j], Zero);
 			if (current_square < min_square || min_square == 0)
 			{
 				min_square = current_square;
@@ -44,4 +38,18 @@ int main()
 	}
 	cout << min_square;
 	return 0;
+}
+
+double distance(dot A, dot B)
+{
+	return sqrt(pow(A.x - B.x, 2) + pow(A.y - B.y,2));
+}
+
+double square_of_triangle(dot A, dot B, dot C)
+{
+	double a = distance(A, B);
+	double b = distance(B, C);
+	double c = distance(A, C);
+	double p = (a + b + c) / 2;
+	return sqrt(p * (p - a) * (p - b) * (p - c));
 }
